160be9e1b33cd365b29ebcd3205b1c862012e801
Fotis Pappas <fotisgpap@grapto.com>
2017-10-19 13:44:16 +0300

    added cloudbust.resources

---
+++ src/Orchard.Web/Modules/CloudBust.Resources/tsconfig.js
@@ -0,0 +1,11 @@
+﻿{
+    "compilerOptions": {
+        "disableSizeLimit": "true",
+        "allowJs": true,
+        "noEmit": true
+    },
+    "typeAcquisition": {
+        "enable": true,
+        "include": ["Scripts"]  //kendo-ui wasn't added via bower, so we need to list it here
+    }
+}
